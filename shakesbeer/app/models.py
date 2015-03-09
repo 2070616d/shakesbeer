7a1c8e36734ee0a2ea5791232bcebd1b57320a0f
unknown <2070616d@BO715-2-04.ad.dcs.gla.ac.uk>
2015-03-09 18:45:13 +0000

    More model work

--- shakesbeer/app/models.py
+++ shakesbeer/app/models.py
@@ -12,6 +12,9 @@ class Recipe(models.Model):
     user = models.ForeignKey(User)
     picture = models.ImageField(blank=True)
     ingredients = models.ManyToManyField(Ingredient, through='UtilisedIngredient')
+    instructions = models.CharField(max_length=1024)
+    avgrating = models.FloatField(default=0.0)
+    noratings = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.name
@@ -21,6 +24,10 @@ class Recipe(models.Model):
             if i == ingredient:
                 return True
         return False
+
+    def applyRating(rating):
+        avgrating = (avgrating*noratings + rating)/(noratings+1)
+        noratings = noratings+1
 
 # django canny store dictionaries inside models, therefore we do stupid things like this
 class UtilisedIngredient(models.Model):
