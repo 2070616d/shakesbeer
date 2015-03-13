b5f699a29d4a4793f9996c3ab0136490b2834cd8
unknown <2070616d@BO706-03.ad.dcs.gla.ac.uk>
2015-03-13 13:48:45 +0000

    search widget, non-functional

--- shakesbeer/app/templatetags/tags.py
+++ shakesbeer/app/templatetags/tags.py
@@ -5,8 +5,12 @@ register = template.Library()
 # mostly for the stars
 @register.filter(name='times')
 def times(number):
-    return range(int(number))
+    return range(int(number+0.5))
 
 @register.filter(name='remainingtimes')
 def remainingtimes(number):
-    return range(5-int(number))
+    return range(5-int(number+0.5))
+
+@register.inclusion_tag('searchbar.html')
+def searchbar(cat=None):
+    return {}
