ce7ac0c1baf1834e9e336b9c1e28e68a457727b4
2092020g <2092020g@BO706-24.ad.dcs.gla.ac.uk>
2015-03-19 16:37:56 +0000

    added two tests for Recipe model

--- shakesbeer/app/tests.py
+++ shakesbeer/app/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from app.models import Recipe, UtilisedIngredient, Comment, Ingredient, Rating
+from django.contrib.auth.models import User
+from datetime import *
 
-# Create your tests here.
+class RecipeMethodTests(TestCase):      
+    def test_slug(self):
+        user = User.objects.create(username="username")
+        instructions = "..."
+        date = datetime.now()
+        picture='/static/images/no-image.png'
+        recipe = Recipe.objects.get_or_create(name="test recipe",instructions=instructions,
+                                      user= user,date=date,picture=picture,avgrating=0.0,noratings=0)[0]
+        self.assertEqual(recipe.slug, 'test-recipeusername')
+
+    def test_unique_slug(self):
+        user = User.objects.create(username="username")
+        instructions = "..."
+        date = datetime.now()
+        picture='/static/images/no-image.png'
+        recipe = Recipe.objects.get_or_create(name="test recipe",instructions=instructions,
+                                      user= user,date=date,picture=picture,avgrating=0.0,noratings=0)[0]
+        # if recipe is saved again "-1" will be added to the slug
+        recipe.save()
+        self.assertEqual(recipe.slug, 'test-recipeusername-1')
